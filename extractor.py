ebcbdbfd0c82924d8b7e564b5aca4f422035e13f
Ted Hsuan Yun Chen <ted.hsuanyun.chen@gmail.com>
2020-08-25 16:27:13 +0300

    initialized code to extract info from raw tweet data

---
+++ extractor.py
@@ -0,0 +1,10 @@
+# Code to extract information from raw twitter data
+
+# retweet edgelists
+# arguments: 1) file location; 2) time period; 3) subsets; 4) output?
+def to_retweets():
+
+
+
+# mentions edgelists
+def to_mentions():
