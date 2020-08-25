a39596e7c73cf1133cdc2d5647def8f43f31e2a3
Ted Hsuan Yun Chen <ted.hsuanyun.chen@gmail.com>
2020-08-25 16:38:46 +0300

    sketched out basic organization for parser functions

--- extractor.py
+++ extractor.py
@@ -1,9 +1,13 @@
 # Code to extract information from raw twitter data
 
-# retweet edgelists
-# arguments: 1) file location; 2) time period; 3) subsets; 4) output?
-def to_retweets():
 
+# top level function 
+# arguments: 1) file location; 2) time period; 3) subsets; 4) output?
+def parse_files(directory, type, period, subset_options, output):
+
+
+# retweet edgelists
+def to_retweets():
 
 
 # mentions edgelists
